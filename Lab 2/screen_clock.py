ead02f98767cb02f46ef4f278705a30b0b9a57d5
Alubeto <rh652@cornell.edu>
2025-09-16 15:04:35 -0400

    Save local edits: update screen_clock and add image-1.png

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -42,6 +42,7 @@ draw = ImageDraw.Draw(image)
 # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 disp.image(image, rotation)
+
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
 padding = -2
@@ -62,10 +63,32 @@ backlight.value = True
 
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
-
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    
+    # Lab 2 part D: Display clock information (from cli_clock.py)
+    current_time = time.strftime("%H:%M:%S")
+    current_date = time.strftime("%m/%d/%Y")
+    
+    # Get text dimensions for centering
+    time_bbox = draw.textbbox((0, 0), current_time, font=font)
+    date_bbox = draw.textbbox((0, 0), current_date, font=font)
+    
+    time_width = time_bbox[2] - time_bbox[0]
+    time_height = time_bbox[3] - time_bbox[1]
+    date_width = date_bbox[2] - date_bbox[0]
+    date_height = date_bbox[3] - date_bbox[1]
+    
+    # Center the time and date on screen
+    time_x = (width - time_width) // 2
+    time_y = (height // 2) - time_height - 10
+    
+    date_x = (width - date_width) // 2  
+    date_y = (height // 2) + 10
+    
+    # Draw the time and date
+    draw.text((time_x, time_y), current_time, font=font, fill=(255, 255, 255))  # White time
+    draw.text((date_x, date_y), current_date, font=font, fill=(0, 255, 0))      # Green date
+    
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
