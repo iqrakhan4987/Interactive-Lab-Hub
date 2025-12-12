fc0d9a271761a3cc5d55656b1b4db04133ad2711
iqrakhan4987 <115332797+iqrakhan4987@users.noreply.github.com>
2025-12-11 22:09:32 -0500

    Simplify LED color setting from serial input
    Refactored LED control logic to simplify color setting from serial input.

--- Final_Project/Led_Control_arduino/Led_Control_arduino.ino
+++ Final_Project/Led_Control_arduino/Led_Control_arduino.ino
@@ -3,42 +3,28 @@
 #define PIN        6
 #define NUMPIXELS  16
 
-// standard setting for WS2812
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial.begin(9600); // Start listening on USB at 9600 speed
+  Serial.begin(9600);
   pixels.begin();
-  pixels.show(); // Clear all pixels to off
+  pixels.show(); // Initialize off
 }
 
 void loop() {
-  // Is the Pi trying to talk to us?
   if (Serial.available() > 0) {
+    // We expect "Red, Green, Blue"
+    int r = Serial.parseInt();
+    int g = Serial.parseInt();
+    int b = Serial.parseInt();
     
-    // Read the incoming string until a newline character
-    String command = Serial.readStringUntil('\n');
-    command.trim(); // Remove whitespace
-    
-    // Parse the command "R,G,B"
-    // Find the positions of the commas
-    int firstComma = command.indexOf(',');
-    int secondComma = command.indexOf(',', firstComma + 1);
-    
-    // If we found both commas, we have valid data
-    if (firstComma > 0 && secondComma > 0) {
-      int r = command.substring(0, firstComma).toInt();
-      int g = command.substring(firstComma + 1, secondComma).toInt();
-      int b = command.substring(secondComma + 1).toInt();
-      
-      setRingColor(r, g, b);
+    // Read the newline
+    if (Serial.read() == '\n') {
+      pixels.setBrightness(255);
+      for(int i=0; i<NUMPIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(r, g, b));
+      }
+      pixels.show();
     }
   }
-}
-
-void setRingColor(int r, int g, int b) {
-  for(int i=0; i<NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(r, g, b));
-  }
-  pixels.show();
 }
